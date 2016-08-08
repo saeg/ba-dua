8888795dd56459f0df5adb670abbbcdf9cf469ac
Roberto Araujo <roberto.andrioli@gmail.com>
2016-08-07 21:13:56 -0300

    A few more validation tests

---
+++ ba-dua-tests/src/test/java/br/usp/each/saeg/badua/test/validation/StaticField3SourceTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2014, 2016 University of Sao Paulo and Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Roberto Araujo - initial API and implementation and/or initial documentation
+ */
+package br.usp.each.saeg.badua.test.validation;
+
+import org.junit.Test;
+
+import br.usp.each.saeg.badua.test.validation.targets.StaticField;
+
+public class StaticField3SourceTest extends ValidationTargetsTest {
+
+    public StaticField3SourceTest() {
+        super(StaticField.class);
+    }
+
+    @Override
+    public final void run(final Class<?> klass) throws Exception {
+        klass.getMethod("start").invoke(null);
+        klass.getMethod("start").invoke(null);
+    }
+
+    @Test
+    public void verifyCoveredDU() {
+        assertTotal(true, 2);
+        assertDU(18, 18, 19, "running", true);
+        assertDU(18, 18, 21, "running", true);
+    }
+
+    @Test
+    public void verifyNotCoveredDU() {
+        assertTotal(false, 0);
+    }
+
+}
