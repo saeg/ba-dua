a51df5c9b213603ed07af56c7fa21d61574d8d01
Roberto Araujo <roberto.andrioli@gmail.com>
2017-02-07 22:14:07 -0300

    Testing exception flow

---
+++ ba-dua-tests/src/test/java/br/usp/each/saeg/badua/test/validation/CatchExceptionSourceTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2014, 2017 University of Sao Paulo and Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Roberto Araujo - initial API and implementation and/or initial documentation
+ */
+package br.usp.each.saeg.badua.test.validation;
+
+import org.junit.Test;
+
+import br.usp.each.saeg.badua.test.validation.targets.CatchException;
+
+public class CatchExceptionSourceTest extends ValidationTargetsTest {
+
+    public CatchExceptionSourceTest() {
+        super(CatchException.class);
+    }
+
+    @Override
+    public final void run(final Class<?> klass) throws Exception {
+        klass.getMethod("run").invoke(null);
+    }
+
+    @Test
+    public void test() {
+        /**
+         * This test includes some wrong assertions. Using the test only to
+         * detect possible unexpected changes in the current behavior
+         *
+         * This is a known limitations due to exception flows
+         *
+         * In some future version we will address these issues
+         */
+        assertTotal(true, 6); // <--- The correct value is 3
+        assertTotal(false, 0); // <--- The correct value is 3
+        assertDU(22, 25, "var", true);
+        assertDU(22, 26, "var", true);
+        assertDU(34, 37, "var", true);
+        assertDU(34, 39, "var", true); // <--- wrong here, exception before the use
+        assertDU(46, 50, "var", true); // <--- wrong here, exception before the use
+        assertDU(46, 51, "var", true); // <--- wrong here, exception before the use
+    }
+
+}
---
+++ ba-dua-tests/src/test/java/br/usp/each/saeg/badua/test/validation/targets/CatchException.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2014, 2017 University of Sao Paulo and Contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Roberto Araujo - initial API and implementation and/or initial documentation
+ */
+package br.usp.each.saeg.badua.test.validation.targets;
+
+public class CatchException {
+
+    public static void run() {
+        catchException1();
+        catchException2();
+        catchException3();
+    }
+
+    private static void catchException1() {
+        var = 0;
+        if (t()) {
+            try {
+                use(var);
+                use(var);
+                ex();
+            } catch (final Ex e) {
+            }
+        }
+    }
+
+    private static void catchException2() {
+        var = 0;
+        if (t()) {
+            try {
+                use(var);
+                ex();
+                use(var);
+            } catch (final Ex e) {
+            }
+        }
+    }
+
+    private static void catchException3() {
+        var = 0;
+        if (t()) {
+            try {
+                ex();
+                use(var);
+                use(var);
+            } catch (final Ex e) {
+            }
+        }
+    }
+
+    private static int var;
+
+    private static void use(final int var) {
+    }
+
+    private static boolean t() {
+        return true;
+    }
+
+    private static void ex() {
+        throw new Ex();
+    }
+
+    public static class Ex extends RuntimeException {
+        private static final long serialVersionUID = 1L;
+    }
+
+}
