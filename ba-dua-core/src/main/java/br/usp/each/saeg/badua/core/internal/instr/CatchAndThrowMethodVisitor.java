4cec4cab0fc1c8ca9b49db28d63b4764fe671bc6
Roberto Araujo <roberto.andrioli@gmail.com>
2017-04-30 22:46:16 -0300

    Following ASM API 'spec' for CatchAndThrowMethodVisitor
    The previous commit is working, but have at least three issues
    that do not respect the ASM API:
    
    - call visitLabel(...) before visitTryCatchBlock(...)
      * accordly to ASM JavaDoc, visitTryCatchBlock(...) can thrown
        IllegalArgumentException if one of the labels has already been
        visited by the method visitLabel(...). Since we used MethodNode
        the exception was not throwed, but we can validate this 'spec'
        with the CheckMethodAdapter.
    
    - call visitLabel/Insn/TryCatchBlock(...) after visitMax(...)
      * accordly to CheckMethodAdapter, none of these methods can be
        called after visitMax(...). Again, with MethodNode nothing
        wrong happens.
    
    -- missing super.visitCode()
    
    We can't simply call visitTryCatchBlock() before visitLabel(start) in
    visitCode() method. Note that this will register a new handler at the
    beginning of the exception table which precedence over all other
    try-catch blocks.

--- ba-dua-core/src/main/java/br/usp/each/saeg/badua/core/internal/instr/CatchAndThrowMethodVisitor.java
+++ ba-dua-core/src/main/java/br/usp/each/saeg/badua/core/internal/instr/CatchAndThrowMethodVisitor.java
@@ -10,9 +10,12 @@
  */
 package br.usp.each.saeg.badua.core.internal.instr;
 
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.Handle;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.TypePath;
 import org.objectweb.asm.tree.MethodNode;
 
 public class CatchAndThrowMethodVisitor extends MethodVisitor {
@@ -25,6 +28,8 @@ public class CatchAndThrowMethodVisitor extends MethodVisitor {
 
     private Label handler;
 
+    private boolean started;
+
     public CatchAndThrowMethodVisitor(final String type, final MethodNode mn) {
         super(Opcodes.ASM5, mn);
         this.type = type;
@@ -35,21 +40,134 @@ public class CatchAndThrowMethodVisitor extends MethodVisitor {
         start = new Label();
         end = new Label();
         handler = new Label();
-        visitLabel(start);
+        started = false;
+        super.visitCode();
     }
 
     @Override
-    public void visitEnd() {
-        visitLabel(end);
-        visitLabel(handler);
-        visitInsn(Opcodes.ATHROW);
-        visitTryCatchBlock(start, end, handler, type);
+    public void visitEnd() { // NOPMD
         super.visitEnd();
     }
 
     @Override
     public void visitMaxs(final int maxStack, final int maxLocals) {
+        visitTryCatchBlockEnd();
         super.visitMaxs(Math.max(maxStack, 1), maxLocals);
+    }
+
+    @Override
+    public void visitFrame(final int type,
+            final int nLocal, final Object[] local,
+            final int nStack, final Object[] stack) {
+        visitTryCatchBlockStart();
+        super.visitFrame(type, nLocal, local, nStack, stack);
+    }
+
+    @Override
+    public void visitInsn(final int opcode) {
+        visitTryCatchBlockStart();
+        super.visitInsn(opcode);
+    }
+
+    @Override
+    public void visitIntInsn(final int opcode, final int operand) {
+        visitTryCatchBlockStart();
+        super.visitIntInsn(opcode, operand);
+    }
+
+    @Override
+    public void visitVarInsn(final int opcode, final int var) {
+        visitTryCatchBlockStart();
+        super.visitVarInsn(opcode, var);
+    }
+
+    @Override
+    public void visitTypeInsn(final int opcode, final String type) {
+        visitTryCatchBlockStart();
+        super.visitTypeInsn(opcode, type);
+    }
+
+    @Override
+    public void visitFieldInsn(final int opcode, final String owner, final String name, final String desc) {
+        visitTryCatchBlockStart();
+        super.visitFieldInsn(opcode, owner, name, desc);
+    }
+
+    @Override
+    @Deprecated
+    public void visitMethodInsn(final int opcode, final String owner, final String name, final String desc) {
+        visitTryCatchBlockStart();
+        super.visitMethodInsn(opcode, owner, name, desc);
+    }
+
+    @Override
+    public void visitMethodInsn(final int opcode, final String owner, final String name, final String desc, final boolean itf) {
+        visitTryCatchBlockStart();
+        super.visitMethodInsn(opcode, owner, name, desc, itf);
+    }
+
+    @Override
+    public void visitInvokeDynamicInsn(final String name, final String desc, final Handle bsm, final Object... bsmArgs) {
+        visitTryCatchBlockStart();
+        super.visitInvokeDynamicInsn(name, desc, bsm, bsmArgs);
+    }
+
+    @Override
+    public void visitJumpInsn(final int opcode, final Label label) {
+        visitTryCatchBlockStart();
+        super.visitJumpInsn(opcode, label);
+    }
+
+    @Override
+    public void visitLabel(final Label label) {
+        visitTryCatchBlockStart();
+        super.visitLabel(label);
+    }
+
+    @Override
+    public void visitLdcInsn(final Object cst) {
+        visitTryCatchBlockStart();
+        super.visitLdcInsn(cst);
+    }
+
+    @Override
+    public void visitIincInsn(final int var, final int increment) {
+        visitTryCatchBlockStart();
+        super.visitIincInsn(var, increment);
+    }
+
+    @Override
+    public void visitMultiANewArrayInsn(final String desc, final int dims) {
+        visitTryCatchBlockStart();
+        super.visitMultiANewArrayInsn(desc, dims);
+    }
+
+    @Override
+    public AnnotationVisitor visitInsnAnnotation(final int typeRef, final TypePath typePath, final String desc, final boolean visible) {
+        visitTryCatchBlockStart();
+        return super.visitInsnAnnotation(typeRef, typePath, desc, visible);
+    }
+
+    @Override
+    public void visitLineNumber(final int line, final Label start) {
+        visitTryCatchBlockStart();
+        super.visitLineNumber(line, start);
+    }
+
+    private void visitTryCatchBlockStart() {
+        if (!started) {
+            started = true;
+            visitTryCatchBlock(start, end, handler, type);
+            visitLabel(start);
+        }
+    }
+
+    private void visitTryCatchBlockEnd() {
+        if (started) {
+            visitLabel(end);
+            visitLabel(handler);
+            visitInsn(Opcodes.ATHROW);
+        }
     }
 
 }
